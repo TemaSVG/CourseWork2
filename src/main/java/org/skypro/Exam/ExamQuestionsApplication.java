39b5e7b4e6965341b4d8fb0cb1b56513668fdf74
Артём <temasvg@gmail.com>
2025-04-12 19:54:12 +1000

    Initial Commit

---
+++ src/main/java/org/skypro/Exam/ExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsApplication.class, args);
+	}
+
+}
